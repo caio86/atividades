50099b14f3cae97d7e6f09d9aac5358f8747361c
caio86 <caioluiz86@gmail.com>
2024-08-24 21:05:27 -0300

    feat: resolve array exercise

--- FullstackWebDev/Frontend/javascript/array/index.js
+++ FullstackWebDev/Frontend/javascript/array/index.js
@@ -1,4 +1,4 @@
-function max(list) {
+function min(list) {
   let min = list[0];
   for (let valor of list) {
     if (valor < min) {
@@ -20,6 +20,11 @@ function max(list) {
 
 function range(start, stop, step = 1) {
   let list = [];
+
+  if (stop == undefined) {
+    stop = start;
+    start = 0;
+  }
 
   if (start < stop) {
     for (let x = start; x < stop; x += step) {
@@ -45,7 +50,7 @@ function uniq(list) {
     }
   }
 
-  return uniq;
+  return sortNum(uniq);
 }
 
 function sortNum(list) {
@@ -66,4 +71,15 @@ function zip(...lists) {
   return [...resultado];
 }
 
+console.log(min([1, 4, 2, 6, 10, 3]));
+console.log(min([1, 4, -1, 6, 10, 3]));
+console.log(max([1, 4, 2, 6, 10, 3]));
+console.log(range(10));
+console.log(range(1, 11));
+console.log(range(0, 30, 5));
 console.log(zip(["moe", "larry"], [30, 40]));
+console.log(zip(["moe", "larry", "curly"], [30, 40, 50], [true, false, false]));
+console.log(uniq([1, 2, 1, 4, 1, 3]));
+console.log(uniq([1, 2, 1, 3, 3]));
+console.log(sortNum([1, 3, 2]));
+console.log(sortNum([1, 2, 10, 3, 32]));
