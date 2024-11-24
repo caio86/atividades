f3e5cc78f9fb8906092ce1fc7553d8a4cc90d1be
caio86 <caioluiz86@gmail.com>
2024-11-24 20:33:58 -0300

    implement withPermission

--- FullstackWebDev/Frontend/react/atividade-profilecard/src/App.js
+++ FullstackWebDev/Frontend/react/atividade-profilecard/src/App.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 import ProfileCard from "./components/ProfileCard";
 
 function App() {
+  const [hasPermission, setHasPermission] = useState(false);
   const userData = require("./userData.json");
+
+  const withPermission = (WrappedComponent) => {
+    return (props) => {
+      if (!props.hasPermission) {
+        return <h1>Permissão negada</h1>;
+      }
+      return <WrappedComponent {...props} />;
+    };
+  };
 
   const appStyle = {
     display: "grid",
@@ -12,12 +23,31 @@ function App() {
     padding: "2rem",
   };
 
-  return (
+  const ProfileList = () => (
     <div style={appStyle}>
       {userData.map((user) => (
         <ProfileCard name={user.name} bio={user.bio} imageUrl={user.imageUrl} />
       ))}
     </div>
+  );
+
+  const ProtecedProfileList = withPermission(ProfileList);
+
+  return (
+    <>
+      <ProtecedProfileList hasPermission={hasPermission} />
+      {!hasPermission ? (
+        <button
+          onClick={() => {
+            setHasPermission(true);
+          }}
+        >
+          Receber autorização
+        </button>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
 
