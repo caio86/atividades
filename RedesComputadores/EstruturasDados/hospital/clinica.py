120de34e50810782c8f772e2d91dffec18c8d6cd
caio86 <caioluiz86@gmail.com>
2025-01-22 22:45:43 -0300

    implement: cadastrar_paciente, consultar_posicao

--- RedesComputadores/EstruturasDados/hospital/clinica.py
+++ RedesComputadores/EstruturasDados/hospital/clinica.py
@@ -1,6 +1,7 @@
-from paciente import Paciente
+from color import Color
 
-from fila import Fila
+from fila import Fila, FilaException
+from paciente import Paciente
 from painel import ItemPainel, Painel
 
 
@@ -14,23 +15,43 @@ class Clinica:
         self.__painel_cadastro = Painel("Cadastro")
         self.__painel_consultorio = Painel("Consultório Médico")
 
+    @classmethod
+    def incrementar_contador(cls):
+        cls.__contador += 1
+
+        if cls.__contador >= 1000:
+            cls.__contador = 1
+
     def obter_senha(self):
-        self.__contador += 1
+        self.incrementar_contador()
+
         senha = f"N{self.__contador:03}"
-        self.__fila_chamada.enfileirar(Paciente(senha))
+        self.__fila_chamada.enfileirar(senha)
         return senha
 
-    def cadastrar_paciente(self, nome: str):
-        paciente = self.__fila_chamada.desenfileirar()
-        self.__painel_cadastro.chamar(ItemPainel(paciente.senha))
-        paciente.nome = nome
-        self.__fila_atendimento.enfileirar(paciente)
+    def cadastrar_paciente(self):
+        try:
+            senha = self.__fila_chamada.desenfileirar()
+            self.__painel_cadastro.chamar(ItemPainel(senha))
+            nome = input("Digite o nome do paciente: ").capitalize()
+            self.__fila_atendimento.enfileirar(Paciente(nome, senha))
+        except FilaException:
+            print("Não há pacientes na fila de cadastro\n\n")
 
     def chamada_consultorio(self):
         pass
 
-    def consultar_posicao(self, senha: str):
-        pass
+    def consultar_posicao(self, senha: str) -> int:
+        if senha.isdecimal():
+            senha = f"N{int(senha):03}"
+
+        try:
+            return self.__fila_chamada.busca(senha)
+        except FilaException:
+            try:
+                return self.__fila_atendimento.busca(senha)
+            except FilaException:
+                return -1
 
     def mostrar_painel(self):
         print(f"\t\t{Color.BOLD}Painel de Atendimento{Color.END}")
--- RedesComputadores/EstruturasDados/hospital/main.py
+++ RedesComputadores/EstruturasDados/hospital/main.py
@@ -1,7 +1,8 @@
 from enum import Enum
 from color import Color
 from clinica import Clinica
-from paciente import Paciente
+
+import os
 
 
 class Opcoes(Enum):
@@ -13,7 +14,12 @@ class Opcoes(Enum):
     Sair = (6, "Sair")
 
 
+def limpaTela():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
 def printMenu():
+    limpaTela()
     res = Color.BOLD + "Clinica Medica - Atendimento" + Color.END + "\n"
 
     res += "=" * 40 + "\n"
@@ -29,22 +35,28 @@ sis = Clinica()
 
 opcao = 0
 
+printMenu()
 while opcao != Opcoes.Sair.value[0]:
-    printMenu()
     opcao = int(input("Digite uma opção: "))
+    printMenu()
 
     if opcao == Opcoes.ObterSenha.value[0]:
         senha = sis.obter_senha()
-        print(f"\n\nSua senha é {Color.CYAN}{senha}{Color.END}\n\n")
+        print(f"Sua senha é {Color.CYAN}{senha}{Color.END}\n\n")
     elif opcao == Opcoes.Cadastrar.value[0]:
-        nome = input("Digite o nome do paciente: ").capitalize()
-        sis.cadastrar_paciente(nome)
+        sis.cadastrar_paciente()
         sis.mostrar_painel()
     elif opcao == Opcoes.Chamar.value[0]:
         sis.chamada_consultorio()
         sis.mostrar_painel()
     elif opcao == Opcoes.ConsultarPosicao.value[0]:
-        senha = input("Digite a senha do paciente: ").upper()
-        sis.consultar_posicao(senha)
+        senha = input(f"{Color.BOLD}Identificação de Senha: {Color.END}").upper()
+        pos = sis.consultar_posicao(senha)
+        if pos == -1:
+            print(
+                f"{Color.BOLD}{Color.RED}A senha, {Color.DARKCYAN}{senha}{Color.RED}, já foi chamada ou não existe{Color.END}\n\n"
+            )
+        else:
+            print(f"{Color.BOLD}{Color.CYAN}{Color.END}")
     elif opcao == Opcoes.Painel.value[0]:
         sis.mostrar_painel()
